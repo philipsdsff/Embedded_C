e062925f4e4c825f9d225636e0c3eb69f15301a5
gigg <ydsfsdfsdf.com>
2023-01-17 12:37:52 +0200

    initial

---
+++ Assignment_Unit3_lesson3/second lab .c/startup.c
@@ -0,0 +1,60 @@
+//startup.c 
+//Eng.philip
+
+
+#include<stdint.h>
+
+void  Reset_Handler();
+extern int main(void);
+void Default_Handler()
+{
+	Reset_Handler();
+}
+void  NMI_Handler()__attribute__((weak,alias("Default_Handler")));;
+void  H_Fault_Handler()__attribute__((weak,alias("Default_Handler")));;
+void  MM_Fault_Handler()__attribute__((weak,alias("Default_Handler")));;
+void  Bus_Handler()__attribute__((weak,alias("Default_Handler")));;
+void  Usage_Fault_Handler()__attribute__((weak,alias("Default_Handler")));;
+
+extern unsigned int stack_top ;
+uint32_t vectors[] __attribute__((section(".vectors")))= {
+	
+(uint32_t) 	&stack_top,
+(uint32_t)	&Reset_Handler,
+(uint32_t)	&NMI_Handler,
+(uint32_t)	&H_Fault_Handler,
+(uint32_t)	&MM_Fault_Handler,
+(uint32_t)	&Bus_Handler,
+(uint32_t)	&Usage_Fault_Handler
+
+};
+extern unsigned int _E_text ;
+extern unsigned int _S_DATA ;
+extern unsigned int _E_DATA ;
+extern unsigned int _S_bss ;
+extern unsigned int _E_bss ;
+void Reset_Handler(void)
+{
+	int i;
+	// انا ششششبعمل بوينتر بيشاور على العنوان فى اللينكر و بحسب الفرق اللى ما بينهم و بعدين كده حسب المساحة اللى محتاجها السيكن ده 
+	
+	// copy .data from flash(ROM) into sram(RAM)
+	unsigned char data_size = (unsigned char*)&_E_DATA  - (unsigned char*)&_S_DATA ;
+	unsigned char* p_src = (unsigned char*)&_E_text ;
+	unsigned char* p_dst = (unsigned char*)&_S_DATA ; //@##
+	for(i = 0; i < data_size; i++)
+	{
+		// بالمساحة اللى حسبها من فرق  العنوانين فى اللينكر  increment بعمل
+		*((unsigned char*)p_dst++) = *((unsigned char*)p_src++);
+		// @##rowبحط استريكز  علشان ادى قابلية للبوينتر انه يكتب عند و يكتب بالبنفس الداتا اللى موجوده فى ال
+	}
+	// init .bss with zero in sram
+	unsigned char bss_size = (unsigned char*)&_E_bss  - (unsigned char*)&_S_bss ;	
+	p_dst = (unsigned char*)&_S_bss ;
+	for(i = 0; i < bss_size; i++)
+	{
+		*((unsigned char*)p_dst++) = (unsigned char)0 ;
+	}
+	// jump to main function
+	main();
+}
